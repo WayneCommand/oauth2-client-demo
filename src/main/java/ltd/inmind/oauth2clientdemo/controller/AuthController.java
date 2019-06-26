273470c2ec01df92e31536448e47c71819052153
shenlanAZ <shenlanluck@gmail.com>
2019-06-26 21:43:04 +0800

    add userservice oauth2 interface and add feign components

--- src/main/java/ltd/inmind/oauth2clientdemo/controller/AuthController.java
+++ src/main/java/ltd/inmind/oauth2clientdemo/controller/AuthController.java
@@ -1,7 +1,17 @@
 package ltd.inmind.oauth2clientdemo.controller;
 
+import feign.Feign;
+import feign.form.FormEncoder;
+import feign.gson.GsonDecoder;
+import feign.okhttp.OkHttpClient;
+import ltd.inmind.oauth2clientdemo.model.AccessTokenResult;
+import ltd.inmind.oauth2clientdemo.service.Oauth2InterfaceService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/oauth/2")
@@ -10,17 +20,65 @@ public class AuthController {
     private String CLIENT_ID = "test";
     private String CLIENT_CODE = "test";
 
+    @Value("${oauth2.redirect-url}")
+    private String redirectUrl;
+
+    @Value("${oauth2.userservice}")
+    private String userservice;
+
+    private Oauth2InterfaceService interfaceService;
+
+    {
+        interfaceService = Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new FormEncoder())
+                .decoder(new GsonDecoder())
+                .target(Oauth2InterfaceService.class, userservice);
+    }
+
+
+
 
     @RequestMapping("/redirect")
-    public String redirect(String code) {
+    public String redirect(String code,HttpServletResponse response) {
 
         //用户已经在认证服务器通过授权
 
         //用client_id,client_code,code申请access_token
+        AccessTokenResult accessToken = interfaceService.accessToken(CLIENT_ID, CLIENT_CODE, code);
 
-        //关联到系统内部用户
+        if (accessToken.getMessage() == null || "".equals(accessToken.getMessage())){
 
-        return "";
+            //TODO 如何和系统内建用户交互
+
+            //登陆
+
+            //跳转
+            try {
+                response.sendRedirect("/");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+            return "redirect error";
+        }else {
+            return "error :" + accessToken.getMessage();
+        }
+
+    }
+
+    @RequestMapping("/userservice")
+    public void userService(HttpServletResponse response){
+
+        String url = String.format("%s/authorize?client_id=%s&redirect_uri=%s/redirect&scope=%s", userservice, CLIENT_ID, redirectUrl, "user info");
+
+        System.out.println("userservice  " + "[String.format] :" + url);
+
+        try {
+            response.sendRedirect(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/ltd/inmind/oauth2clientdemo/service/Oauth2InterfaceService.java
+++ src/main/java/ltd/inmind/oauth2clientdemo/service/Oauth2InterfaceService.java
@@ -1,11 +1,14 @@
 package ltd.inmind.oauth2clientdemo.service;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
+import ltd.inmind.oauth2clientdemo.model.AccessTokenResult;
 
 
 public interface Oauth2InterfaceService {
 
     @RequestLine("POST /access_token")
-    String accessToken(@Param("client_id") String clientId, @Param("client_secret") String clientSecret, @Param("code") String code);
+    @Headers({"Content-Type: application/x-www-form-urlencoded", "Accept: application/json"})
+    AccessTokenResult accessToken(@Param("client_id") String clientId, @Param("client_secret") String clientSecret, @Param("code") String code);
 }
